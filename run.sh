5f5b2b0b4e0f99e696bddbbb00c7201ec744dc32
Don Morrison <dfm@ringing.org>
2025-04-14 15:17:18 -0400

    make sure we're in the right package in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 # Copyright 2025 Carnegie Mellon University
 
-sbcl --load act-up-v1_3_2 --load 'evacuation-model-v0.3.1.lisp' --load model-server --eval "(scale:run nil $1)"
+sbcl --eval "(in-package :cl-user)" --load act-up-v1_3_2 --load 'evacuation-model-v0.3.1.lisp' --load model-server --eval "(scale:run nil $1)"
