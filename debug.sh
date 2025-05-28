8e0ec6861176f4ad16433b392bc5d7684aa31e9f
Don Morrison <dfm@ringing.org>
2025-05-28 11:15:22 -0400

    use the latest model

--- debug.sh
+++ debug.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 # Copyright 2025 Carnegie Mellon University
 
-sbcl --dynamic-space-size 20000 --eval "(in-package :cl-user)" --load act-up-v1_3_2 --load 'evacuation-model-v0.3.2.lisp' --load model-server --eval "(progn (vom:config t :debug) (scale:run nil $1))"
+sbcl --dynamic-space-size 20000 --eval "(in-package :cl-user)" --load act-up-v1_3_2 --load 'evacuation-model-v0.3.3.lisp' --load model-server --eval "(progn (vom:config t :debug) (scale:run nil $1))"
