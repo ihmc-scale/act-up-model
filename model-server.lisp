a18f7ddaafa92a5c7ca66259b76d0b787d05811f
Don Morrison <dfm@ringing.org>
2026-03-30 11:10:34 -0400

    fix some git misery

--- model-server.lisp
+++ model-server.lisp
@@ -35,103 +35,22 @@
         ((floatp jzon) (coerce jzon *read-default-float-format*))
         (t jzon)))
 
-<<<<<<< HEAD
-(defun %run-model (parameters raw-data)
-  (vom:debug "Calling model on ~S ~S" parameters raw-data)
-  (multiple-value-bind (result name)
-      (cl-user::run-model parameters raw-data)
-    (vom:debug "Model returned (~A) ~S" name result)
-    ;; canonicalize result into a list of lists
-    (when (arrayp result)
-      (setf result
-            (case (array-rank result)
-              (1 (coerce result 'list))
-              (2 (iter (for i :from 0 :below (array-dimension result 0))
-                       (collect
-                           (iter (for j :from 0 :below (array-dimension result 1))
-                                 (for x := (aref result i j))
-                                 (when x
-                                   (collect x))))))
-              (t (error "Don't know how to process a ~D dimensional array for return to reasoner"
-                        (array-rank result))))))
-    (values (mapcar (lambda (x) (coerce x 'list)) result) name)))
-
-(defun restructure-input (model-data)
-  (values (iter (for p :in (cdr (assoc :parameters model-data)))
-                (for n := (cdr (assoc :name p)))
-                (assert (stringp n))
-                (setf n (make-keyword (substitute #\- #\Space (string-upcase n))))
-                (collect (cons n (iter (for (k . v) :in p)
-                                       (assert (keywordp k))
-                                       (when (and (eq k :value)
-                                                  (member n '(:utility :similarity)))
-                                         (setf v (iter (for (typ . fn) :in v)
-                                                       (nconcing (list typ (intern (string-upcase fn)
-                                                                                   *data-name-package*))))))
-                                       (unless (eq k :name)
-                                         (nconcing (list k v)))))))
-          (cdr (assoc :raw-data model-data))))
-
-(defun lower-underscore (s)
-  (if (typep s 'string-designator)
-      (substitute-if #\_ (lambda (c) (member c '(#\Space #\-))) (string-downcase s))
-      s))
-
-(defun model-name-to-lisp (s)
-  (assert (typep s 'string-designator))
-  (intern (string-upcase (substitute #\- #\Space (remove #\* (remove \+ (camel-case-to-lisp s)))))
-          *data-name-package*))
-
-(defun restructure-output (data)
-  (iter (for outer :in data)
-        (collect (iter (for inner :in outer)
-                       (collect (iter (for (n . rest) :in inner)
-                                      (collect `((:name . ,(lower-underscore n))
-                                                 ,@(iter (for (k v) :on rest :by #'cddr)
-                                                         (collect (cons k (lower-underscore v))))))))))))
-
-(defun read-process-files (list)
-  (iter (for fname :in list)
-        (unless (find #\. fname)
-          (setf fname (format nil "~a.lisp" fname)))
-        (collect
-         (with-open-file (s (merge-pathnames fname +process-directory+))
-           (iter (with *package* := *data-name-package*)
-                 (for form := (read s nil :eof))
-                 (until (eq form :eof))
-                 (collect form))))))
-
-=======
 (defun read-json (string)
   (let* ((result (listify-jzon (jzon:parse string)))
          (processes (assoc :processes result)))
     (when processes
       (setf (second processes) (mapcar (rcurry #'symbolify nil) (second processes))))
     result))
->>>>>>> 28d73dc (latest ACT-Up stuff and model from Christian; simplify model-server code to match)
 
 (defun handle-line (line)
   (let ((json (read-json line)))
-    (vom:debug "Processing JSON ~S" json)
+    (v:debug "Processing JSON ~S" json)
     (assert (listp json))
-    "return value"))
+    (let ((result (run-model json)))
+      (v:debug "run-model returned ~S" result)
+      (jzon:stringify result))))
 
 (defun tcp-handler (stream)
-<<<<<<< HEAD
-  (vom:info "Connected from ~A" *remote-host*)
-  (iter (for line := (read-line stream nil :eof))
-        (vom:debug "Read line ~S" line)
-        (until (eq line :eof))
-        (format stream "~A~%"
-                (handler-case
-                    (handle-line line)
-                  ((and error #+SBCL (not sb-sys:interactive-interrupt)) (e)
-                    (vom:error "~A (while processing ~S)" e line)
-                    (format stream "Error: ~A~%" e)
-                    (finish-output stream)
-                    (next-iteration)))))
-  (finish-output stream)
-=======
   (v:info "Connected from ~A" *remote-host*)
   (iter (for line := (read-line stream nil '#0=#:eof))
         (v:debug "Read line ~S" line)
@@ -143,12 +62,10 @@
                                   (finish-output stream)
                                   (next-iteration))))
         (finish-output stream))
->>>>>>> 28d73dc (latest ACT-Up stuff and model from Christian; simplify model-server code to match)
   #+SBCL (sb-ext:gc :full t)
   (v:info "Finished from ~A" *remote-host*))
 
-
-(defun run (&optional(interactive (member :swank *features*)) (port +default-port+))
+(defun run (&optional (interactive (member :swank *features*)) (port +default-port+))
   (v:info "Starting SCALE model listener on port ~D" port)
   (labels ((quit (n)
              (unless interactive
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 # Copyright 2025-2026 Carnegie Mellon University
 
-sbcl --dynamic-space-size 20000 --eval "(in-package :cl-user)" --load act-up-v1_3_3 --load 'ACT-UP procedural module.lisp' --load 'evacuation-model-v2.0' --load model-server --eval "(scale:run nil $1)"
+sbcl --dynamic-space-size 20000 --eval "(in-package :cl-user)" --load act-up-v1_3_3 --load 'ACT-UP procedural module.lisp' --load 'evacuation-model-v2.0.lisp' --load model-server --eval "(scale:run nil $1)"
